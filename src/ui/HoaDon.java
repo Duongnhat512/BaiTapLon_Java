20d5234b4b29cbf35ba7967f7369ca8509b399df
Duongnhat512 <duongnhat512@gmail.com>
2023-04-12 16:37:51 +0700

    Hoan thanh giao dien Hoa don va Nha cung cap

--- src/ui/HoaDon.java
+++ src/ui/HoaDon.java
@@ -14,6 +14,16 @@ import javax.swing.JTabbedPane;
 import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import java.awt.BorderLayout;
+import javax.swing.border.TitledBorder;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.border.EtchedBorder;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class HoaDon extends JFrame {
 
@@ -21,6 +31,16 @@ public class HoaDon extends JFrame {
 	private final JPanel pWest = new JPanel();
 	private JTable tableHoaDon;
 	private JTable tableChiTietHD;
+	private JTextField txtMaHD;
+	private JTextField txtTenKH;
+	private JTextField txtNgayLap;
+	private JTextField txtNgayGiao;
+	private JTextField txtNoiNhan;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField txtsoLuong;
+	private JTextField txtGiaBan;
 
 	/**
 	 * Launch the application.
@@ -43,24 +63,26 @@ public class HoaDon extends JFrame {
 	 */
 	public HoaDon() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1100, 718);
+		setTitle("Hóa đơn");
+		setBounds(100, 100, 1200, 680);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		pWest.setBounds(0, 0, 120, 671);
+		pWest.setBounds(10, 10, 110, 623);
 		contentPane.add(pWest);
 		pWest.setLayout(null);
 		
 		JPanel pCenter = new JPanel();
-		pCenter.setBounds(130, 0, 956, 671);
+		pCenter.setBorder(new LineBorder(new Color(0, 0, 0)));
+		pCenter.setBounds(130, 10, 1046, 623);
 		contentPane.add(pCenter);
 		pCenter.setLayout(null);
 		
 		JPanel pHoaDon = new JPanel();
-		pHoaDon.setBounds(0, 10, 946, 316);
+		pHoaDon.setBounds(10, 24, 675, 302);
 		pCenter.add(pHoaDon);
 		pHoaDon.setLayout(new BorderLayout(0, 0));
 		
@@ -77,22 +99,20 @@ public class HoaDon extends JFrame {
 			new Object[][] {
 			},
 			new String[] {
-				"M\u00E3 h\u00F3a \u0111\u01A1n", "Kh\u00E1ch h\u00E0ng", "Nh\u00E2n vi\u00EAn", "Ng\u00E0y l\u1EADp h\u00F3a \u0111\u01A1n", "Ng\u00E0y giao d\u1EF1 ki\u1EBFn", " Ng\u00E0y chuy\u1EC3n h\u00E0ng", "Ng\u00E0y chuy\u1EC3n h\u00E0ng", " N\u01A1i nh\u1EADn h\u00E0ng", "T\u1ED5ng ti\u1EC1n h\u00F3a \u0111\u01A1n"
+				"M\u00E3 h\u00F3a \u0111\u01A1n", "Kh\u00E1ch h\u00E0ng", "Nh\u00E2n vi\u00EAn", "Ng\u00E0y l\u1EADp h\u00F3a \u0111\u01A1n", "Ng\u00E0y giao", "N\u01A1i nh\u1EADn h\u00E0ng", "T\u1ED5ng ti\u1EC1n"
 			}
 		) {
 			Class[] columnTypes = new Class[] {
-				String.class, String.class, String.class, String.class, String.class, String.class, Object.class, String.class, Object.class
+				String.class, String.class, String.class, Object.class, Object.class, String.class, Double.class
 			};
 			public Class getColumnClass(int columnIndex) {
 				return columnTypes[columnIndex];
 			}
 		});
-		tableHoaDon.getColumnModel().getColumn(3).setPreferredWidth(95);
-		tableHoaDon.getColumnModel().getColumn(8).setPreferredWidth(95);
 		scrollPane.setViewportView(tableHoaDon);
 		
 		JPanel pChiTietHD = new JPanel();
-		pChiTietHD.setBounds(0, 336, 775, 325);
+		pChiTietHD.setBounds(10, 354, 675, 259);
 		pCenter.add(pChiTietHD);
 		pChiTietHD.setLayout(new BorderLayout(0, 0));
 		
@@ -121,13 +141,184 @@ public class HoaDon extends JFrame {
 		});
 		scrollPane_1.setViewportView(tableChiTietHD);
 		
+		JPanel pTacVuHD = new JPanel();
+		pTacVuHD.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Ch\u1ECDn t\u00E1c v\u1EE5", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		pTacVuHD.setBounds(695, 116, 341, 240);
+		pCenter.add(pTacVuHD);
+		pTacVuHD.setLayout(null);
+		
+		JLabel lblMaHD = new JLabel("Mã hóa đơn:");
+		lblMaHD.setBounds(6, 17, 150, 21);
+		pTacVuHD.add(lblMaHD);
+		
+		txtMaHD = new JTextField();
+		txtMaHD.setBounds(156, 17, 150, 21);
+		pTacVuHD.add(txtMaHD);
+		txtMaHD.setColumns(10);
+		
+		JLabel lblTenKH = new JLabel("Tên khách hàng:");
+		lblTenKH.setBounds(6, 48, 150, 21);
+		lblTenKH.setHorizontalAlignment(SwingConstants.LEFT);
+		pTacVuHD.add(lblTenKH);
+		
+		txtTenKH = new JTextField();
+		txtTenKH.setBounds(156, 48, 150, 21);
+		pTacVuHD.add(txtTenKH);
+		txtTenKH.setColumns(10);
+		
+		JLabel lblTenNV = new JLabel("Nhân viên bán: ");
+		lblTenNV.setBounds(6, 79, 150, 21);
+		pTacVuHD.add(lblTenNV);
+		
+		JLabel lblNgayLapHD = new JLabel("Ngày lập hóa đơn");
+		lblNgayLapHD.setBounds(6, 110, 150, 21);
+		pTacVuHD.add(lblNgayLapHD);
+		
+		txtNgayLap = new JTextField();
+		txtNgayLap.setBounds(156, 110, 150, 21);
+		pTacVuHD.add(txtNgayLap);
+		txtNgayLap.setColumns(10);
+		
+		JLabel lblNgayGiao = new JLabel("Ngày giao");
+		lblNgayGiao.setBounds(6, 141, 150, 21);
+		pTacVuHD.add(lblNgayGiao);
+		
+		txtNgayGiao = new JTextField();
+		txtNgayGiao.setBounds(156, 141, 150, 21);
+		pTacVuHD.add(txtNgayGiao);
+		txtNgayGiao.setColumns(10);
+		
+		JLabel lblNoiNhan = new JLabel("Nơi nhận");
+		lblNoiNhan.setBounds(6, 172, 150, 21);
+		pTacVuHD.add(lblNoiNhan);
+		
+		txtNoiNhan = new JTextField();
+		txtNoiNhan.setBounds(156, 172, 150, 21);
+		txtNoiNhan.setText("");
+		pTacVuHD.add(txtNoiNhan);
+		txtNoiNhan.setColumns(10);
+		
+		JButton btnThem = new JButton("Thêm");
+		btnThem.setBounds(6, 211, 81, 21);
+		pTacVuHD.add(btnThem);
+		
+		JButton btnXoa = new JButton("Xóa");
+		btnXoa.setBounds(97, 211, 59, 21);
+		pTacVuHD.add(btnXoa);
+		
+		JButton btnSua = new JButton("Sửa");
+		btnSua.setBounds(166, 211, 58, 21);
+		pTacVuHD.add(btnSua);
+		
+		JComboBox cboNhanVien = new JComboBox();
+		cboNhanVien.setEditable(true);
+		cboNhanVien.setBounds(156, 79, 150, 21);
+		pTacVuHD.add(cboNhanVien);
+		
+		JButton btnXoaTrang = new JButton("Xóa trắng");
+		btnXoaTrang.setBounds(234, 211, 93, 21);
+		pTacVuHD.add(btnXoaTrang);
+		
 		JPanel panel = new JPanel();
-		panel.setBounds(785, 336, 161, 325);
+		panel.setBorder(new TitledBorder(null, "T\u00ECm h\u00F3a \u0111\u01A1n theo m\u00E3", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(695, 24, 341, 82);
 		pCenter.add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(10, 10, 141, 30);
-		panel.add(lblNewLabel);
+		JLabel lblTimHD = new JLabel("Nhập mã hóa đơn: ");
+		lblTimHD.setBounds(10, 10, 116, 38);
+		panel.add(lblTimHD);
+		
+		textField = new JTextField();
+		textField.setBounds(125, 20, 176, 19);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnTim = new JButton("Tìm");
+		btnTim.setBounds(173, 53, 88, 19);
+		panel.add(btnTim);
+		
+		JPanel pTacVuCTHD = new JPanel();
+		pTacVuCTHD.setLayout(null);
+		pTacVuCTHD.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Ch\u1ECDn t\u00E1c v\u1EE5", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		pTacVuCTHD.setBounds(695, 383, 341, 230);
+		pCenter.add(pTacVuCTHD);
+		
+		JLabel lblMaCTHD = new JLabel("Mã chi tiết HD:");
+		lblMaCTHD.setBounds(6, 17, 150, 21);
+		pTacVuCTHD.add(lblMaCTHD);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(156, 17, 150, 21);
+		pTacVuCTHD.add(textField_1);
+		
+		JLabel lblMaHD_1 = new JLabel("Mã hóa đơn");
+		lblMaHD_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblMaHD_1.setBounds(6, 48, 150, 21);
+		pTacVuCTHD.add(lblMaHD_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setEditable(false);
+		textField_2.setColumns(10);
+		textField_2.setBounds(156, 48, 150, 21);
+		pTacVuCTHD.add(textField_2);
+		
+		JLabel lblLoai = new JLabel("Loại sản phẩm");
+		lblLoai.setBounds(6, 79, 150, 21);
+		pTacVuCTHD.add(lblLoai);
+		
+		JLabel lblSanPham = new JLabel("Sản phẩm");
+		lblSanPham.setBounds(6, 110, 150, 21);
+		pTacVuCTHD.add(lblSanPham);
+		
+		JLabel lblSoLuong = new JLabel("Ngày giao");
+		lblSoLuong.setBounds(6, 141, 150, 21);
+		pTacVuCTHD.add(lblSoLuong);
+		
+		txtsoLuong = new JTextField();
+		txtsoLuong.setColumns(10);
+		txtsoLuong.setBounds(156, 141, 150, 21);
+		pTacVuCTHD.add(txtsoLuong);
+		
+		JLabel lblGiaBan = new JLabel("Nơi nhận");
+		lblGiaBan.setBounds(6, 172, 150, 21);
+		pTacVuCTHD.add(lblGiaBan);
+		
+		txtGiaBan = new JTextField();
+		txtGiaBan.setText("");
+		txtGiaBan.setColumns(10);
+		txtGiaBan.setBounds(156, 172, 150, 21);
+		pTacVuCTHD.add(txtGiaBan);
+		
+		JButton btnThemCT = new JButton("Thêm");
+		btnThemCT.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnThemCT.setBounds(6, 203, 80, 21);
+		pTacVuCTHD.add(btnThemCT);
+		
+		JButton btnXoaCT = new JButton("Xóa");
+		btnXoaCT.setBounds(96, 203, 60, 21);
+		pTacVuCTHD.add(btnXoaCT);
+		
+		JButton btnSuaCT = new JButton("Sửa");
+		btnSuaCT.setBounds(166, 203, 59, 21);
+		pTacVuCTHD.add(btnSuaCT);
+		
+		JComboBox cboLoai = new JComboBox();
+		cboLoai.setEditable(true);
+		cboLoai.setBounds(156, 79, 150, 21);
+		pTacVuCTHD.add(cboLoai);
+		
+		JComboBox cboSanPham = new JComboBox();
+		cboSanPham.setEditable(true);
+		cboSanPham.setBounds(156, 110, 150, 21);
+		pTacVuCTHD.add(cboSanPham);
+		
+		JButton btnXoaTrangCT = new JButton("Xóa trắng");
+		btnXoaTrangCT.setBounds(235, 203, 88, 21);
+		pTacVuCTHD.add(btnXoaTrangCT);
 	}
 }
--- src/ui/NhaCungCap.java
+++ src/ui/NhaCungCap.java
@@ -5,10 +5,31 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.border.TitledBorder;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class NhaCungCap extends JFrame {
 
 	private JPanel contentPane;
+	private JTable tableNCC;
+	private JTextField txtMaNCC;
+	private JTextField txtTenNCC;
+	private JTextField txtDiaChi;
+	private JTextField txtSoDienThoai;
+	private JTextField txtEmail;
+	private JTextField txtTimNCC;
 
 	/**
 	 * Launch the application.
@@ -31,11 +52,152 @@ public class NhaCungCap extends JFrame {
 	 */
 	public NhaCungCap() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setTitle("Nhà cung cấp");
+		setBounds(100, 100, 1200, 720);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel pWest = new JPanel();
+		pWest.setBounds(10, 10, 110, 670);
+		contentPane.add(pWest);
+		
+		JPanel pCenter = new JPanel();
+		pCenter.setBorder(new LineBorder(new Color(0, 0, 0)));
+		pCenter.setBounds(130, 10, 1046, 670);
+		contentPane.add(pCenter);
+		pCenter.setLayout(null);
+		
+		JPanel pNhaCungCap = new JPanel();
+		pNhaCungCap.setBounds(10, 6, 637, 654);
+		pCenter.add(pNhaCungCap);
+		pNhaCungCap.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblTitleNCC = new JLabel("NHÀ CUNG CẤP");
+		lblTitleNCC.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitleNCC.setFont(new Font("Arial", Font.BOLD, 23));
+		pNhaCungCap.add(lblTitleNCC, BorderLayout.NORTH);
+		lblTitleNCC.setPreferredSize(new Dimension(0, 30));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		pNhaCungCap.add(scrollPane, BorderLayout.CENTER);
+		
+		tableNCC = new JTable();
+		tableNCC.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"M\u00E3 nh\u00E0 cung c\u1EA5p", "T\u00EAn nh\u00E0 cung c\u1EA5p", "\u0110\u1ECBa ch\u1EC9", "S\u1ED1 \u0111i\u1EC7n tho\u1EA1i", "Email"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, Object.class, String.class, Object.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		tableNCC.setFont(new Font("Arial", Font.BOLD, 10));
+		scrollPane.setViewportView(tableNCC);
+		
+		JPanel pTacVu = new JPanel();
+		pTacVu.setBorder(new TitledBorder(null, "Ch\u1ECDn t\u00E1c v\u1EE5", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pTacVu.setBounds(657, 10, 379, 402);
+		pCenter.add(pTacVu);
+		pTacVu.setLayout(null);
+		
+		JPanel pThongTin = new JPanel();
+		pThongTin.setBounds(10, 35, 359, 192);
+		pTacVu.add(pThongTin);
+		pThongTin.setLayout(null);
+		
+		JLabel lblMaNCC = new JLabel("Mã nhà cung cấp: ");
+		lblMaNCC.setBounds(0, 1, 107, 30);
+		pThongTin.add(lblMaNCC);
+		
+		txtMaNCC = new JTextField();
+		txtMaNCC.setBounds(106, 1, 252, 30);
+		txtMaNCC.setText("");
+		pThongTin.add(txtMaNCC);
+		txtMaNCC.setColumns(10);
+		
+		JLabel lblTenNCC = new JLabel("Tên nhà cung cấp:");
+		lblTenNCC.setBounds(0, 41, 107, 30);
+		pThongTin.add(lblTenNCC);
+		
+		txtTenNCC = new JTextField();
+		txtTenNCC.setBounds(106, 41, 252, 30);
+		pThongTin.add(txtTenNCC);
+		txtTenNCC.setColumns(10);
+		
+		JLabel lblDiaChi = new JLabel("Địa chỉ:");
+		lblDiaChi.setBounds(0, 81, 107, 30);
+		pThongTin.add(lblDiaChi);
+		
+		txtDiaChi = new JTextField();
+		txtDiaChi.setBounds(106, 81, 252, 30);
+		pThongTin.add(txtDiaChi);
+		txtDiaChi.setColumns(10);
+		
+		JLabel lblSoDienThoai = new JLabel("Số điện thoại:");
+		lblSoDienThoai.setBounds(0, 121, 107, 30);
+		pThongTin.add(lblSoDienThoai);
+		
+		txtSoDienThoai = new JTextField();
+		txtSoDienThoai.setBounds(106, 121, 252, 30);
+		txtSoDienThoai.setText("");
+		pThongTin.add(txtSoDienThoai);
+		txtSoDienThoai.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setBounds(0, 161, 107, 30);
+		pThongTin.add(lblEmail);
+		
+		txtEmail = new JTextField();
+		txtEmail.setBounds(106, 161, 252, 30);
+		pThongTin.add(txtEmail);
+		txtEmail.setColumns(10);
+		
+		JButton btnThem = new JButton("Thêm");
+		btnThem.setBounds(34, 255, 120, 36);
+		pTacVu.add(btnThem);
+		
+		JButton btnXoa = new JButton("Xóa");
+		btnXoa.setBounds(218, 255, 120, 36);
+		pTacVu.add(btnXoa);
+		
+		JButton btnSua = new JButton("Sửa");
+		btnSua.setBounds(34, 332, 120, 36);
+		pTacVu.add(btnSua);
+		
+		JButton btnXoaTrang = new JButton("Xóa trắng");
+		btnXoaTrang.setBounds(218, 332, 120, 36);
+		pTacVu.add(btnXoaTrang);
+		
+		JPanel pTimKiem = new JPanel();
+		pTimKiem.setBorder(new TitledBorder(null, "T\u00ECm ki\u1EBFm", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		pTimKiem.setBounds(657, 441, 379, 219);
+		pCenter.add(pTimKiem);
+		pTimKiem.setLayout(null);
+		
+		JLabel lblTim = new JLabel("Nhập tên nhà cung cấp cần tìm:");
+		lblTim.setBounds(10, 35, 224, 35);
+		pTimKiem.add(lblTim);
+		
+		txtTimNCC = new JTextField();
+		txtTimNCC.setBounds(20, 88, 334, 35);
+		pTimKiem.add(txtTimNCC);
+		txtTimNCC.setColumns(10);
+		
+		JButton btnTim = new JButton("Tìm");
+		btnTim.setBounds(54, 152, 112, 44);
+		pTimKiem.add(btnTim);
+		
+		JButton btnXoaTrangTim = new JButton("Xóa trắng");
+		btnXoaTrangTim.setBounds(221, 152, 112, 44);
+		pTimKiem.add(btnXoaTrangTim);
 	}
-
 }
